8edc6171bc9fe0eb8a67c91b102f8f4ec81e5ffd
saarrozen <saarroze0@gmail.com>
2022-11-08 16:26:38 +0200

    finish add new members

---
+++ system/members/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
+
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth.models import User
+
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+def logout_user(request):
+    logout(request)
+    return Response("logout", status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@renderer_classes([JSONRenderer])
+def login_user(request):
+    username = request.data['username']
+    password = request.data['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return Response(status=status.HTTP_200_OK)
+    else:
+        return Response("You did not connect, try again")
+
+
+@api_view(['POST'])
+@renderer_classes([JSONRenderer])
+def register_user(request):
+    username = request.data['username']
+    password = request.data['password']
+    User.objects.create_user(username=username, password=password)
+    return Response("You have successfully registered", status=status.HTTP_200_OK)
--- system/message/urls.py
+++ system/message/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # write message
+    
     path('sendMessage/', views.sendMessage),
-
-    # Get all messages for a specific user
-    path('inbox/<str:user>', views.inbox),
-    path('unread-inbox/<str:user>', views.unread_inbox),
+    path('inbox/', views.inbox),
+    path('unread-inbox/', views.unread_inbox),
     path('readMessage/<int:id>', views.readMessage),
     path('deleteMessage/<str:user>/<int:id>', views.deleteMessage),
+
 ]
