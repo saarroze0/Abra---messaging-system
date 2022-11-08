83656e4b4839b794ce884a11433c9df744cfdfa1
saarrozen <saarroze0@gmail.com>
2022-11-08 18:13:48 +0200

    finish

--- system/system/settings.py
+++ system/system/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # LOGIN_REDIRECT_URL = '/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 class HerokuDiscoverRunner(DiscoverRunner):
